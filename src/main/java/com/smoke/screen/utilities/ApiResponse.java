a063d16f9558385ce912ad97622715ec9bc6f381
Vishwas <im.vishwas.03@gmail.com>
2025-11-10 23:31:07 +0530

    utilities

---
+++ src/main/java/com/smoke/screen/utilities/ApiResponse.java
@@ -0,0 +1,34 @@
+package com.smoke.screen.utilities;
+
+public class ApiResponse {
+
+	private String message;
+	private boolean success;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public ApiResponse() {
+		super();
+	}
+
+	public ApiResponse(String message, boolean success) {
+		super();
+		this.message = message;
+		this.success = success;
+	}
+
+}
---
+++ src/main/java/com/smoke/screen/utilities/JwtAuthResponse.java
@@ -0,0 +1,27 @@
+package com.smoke.screen.utilities;
+
+import com.smoke.screen.entities.User;
+
+public class JwtAuthResponse {
+
+	private String token;
+	
+	private User user;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+}
